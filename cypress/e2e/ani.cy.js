a5fe3d31748174c8ecd38e2b86030251ea9c9d6b
anikrk <akirkitadze96@gmail.com>
2025-04-04 12:34:43 +0400

    Delete cypress/e2e/ani.cy.js

--- cypress/e2e/ani.cy.js
+++
@@ -1,9 +0,0 @@
-const { should } = require("chai")
-
-describe('template spec', () => {
-  it('register user', () => {
-    cy.visit('https://automationexercise.com/')
-    cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
-    cy.get('.signup-form > h2').should("have.text", "New User Signup!");
-  })
-})
